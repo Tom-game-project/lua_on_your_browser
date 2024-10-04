0c7fd014e07106f7d4e3e0f64ac713b5f13be6d9
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2024-10-04 14:55:59 +0900

    add arg to `func` function

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
 use wasm_bindgen::prelude::*;
 use piccolo::{Lua, Callback, Closure, CallbackReturn, StaticError, Value, Executor};
 
-fn func()-> Result<(i32, i32, i32), StaticError>{
-        let mut lua = Lua::core();
+use std::io::Cursor;
+
+fn func(code:String)-> Result<(i32, i32, i32), StaticError>{
+    let cursor = Cursor::new(code);
+
+    let mut lua = Lua::core();
 
     lua.try_enter(|ctx| {
         let callback = Callback::from_fn(&ctx, |_, _, mut stack| {
@@ -17,13 +21,7 @@ fn func()-> Result<(i32, i32, i32), StaticError>{
         let closure = Closure::load(
             ctx,
             None,
-            &br#"
-                local a, b, c = callback(1, 2)
-                assert(a == 1 and b == 2 and c == 42)
-                local d, e, f = callback(3, 4)
-                assert(d == 3 and e == 4 and f == 42)
-                return a,b,c
-            "#[..],
+            cursor,
         )?;
 
         Ok(ctx.stash(Executor::start(ctx, closure.into(), ())))
@@ -33,13 +31,15 @@ fn func()-> Result<(i32, i32, i32), StaticError>{
     Ok((a, b, c))
 }
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
 #[wasm_bindgen]
 pub fn wasm_func(){
-    let (a,b,c) = func().unwrap();
+    let (a,b,c) = func(r#"
+    local a, b, c = callback(1, 2)
+    assert(a == 1 and b == 2 and c == 42)
+    local d, e, f = callback(3, 4)
+    assert(d == 3 and e == 4 and f == 42)
+    return a,b,c
+    "#.to_string()).unwrap();
     gloo::console::log!(format!("{},{},{}",a,b,c));
 }
 
@@ -48,13 +48,13 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-
-    #[test]
     fn it_works1() {
-        func();
+        func(r#"
+    local a, b, c = callback(1, 2)
+    assert(a == 1 and b == 2 and c == 42)
+    local d, e, f = callback(3, 4)
+    assert(d == 3 and e == 4 and f == 42)
+    return a,b,c
+    "#.to_string());
     }
 }
