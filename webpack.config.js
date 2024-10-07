c474f1e27e975c9e25b2a0eedf78638ce7aafeb0
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2024-10-07 13:58:07 +0900

    [WIP] fix distination

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './index.js', // エントリーポイントのファイルパス
   output: {
-    publicPath:"dist/",
+    publicPath:"",
     // publicPath: '',
     path: path.resolve(__dirname, 'dist'), // 出力先ディレクトリのパス
     filename: 'main.js', // 出力ファイル名
